30db8090233f8a1a0623644e95f4014ed4f2f316
Brian Kloppenborg <brian@kloppenborg.net>
2011-09-12 20:05:18 +0000

    Updated submit_fitast.cmd for epsAur data.
    git-svn-id: http://192.168.1.75/svn/orbit-fit@43 b9008261-a95d-470b-9c38-38f0b39c50e0

--- bin/submit_fitast.cmd
+++ bin/submit_fitast.cmd
@@ -17,4 +17,4 @@ echo Dir: $PWD
 echo This jobs runs on the following processors:
 cat $PBS_NODEFILE
 # openmpi test
-mpirun -np 16 -machinefile $PBS_NODEFILE ./fitast ./epsAur_reformat_fit_pm.txt -units deg -motion -x0_min 75 -x0_max 76 -y0_min 43 -y0_max 44 -alpha_min 0 -alpha_max 30
+mpirun -np 16 -machinefile $PBS_NODEFILE ./fitast ../test_data/epsAur_ast_refit-pm.txt -tau_min 2428900 -tau_max 2442800 -T_min 9500 -T_max 10500 -alpha_min 1E-7 -alpha_max 1E-4 -e_min 0.252 -e_max 0.283 -inc_min 80 -inc_max 100 -omega_min 33 -omega_max 40 -motion -x0_min 75.4920 -x0_max 75.4923 -y0_min 43.8232 -y0_max 43.8235 -mu_x_min -1E-8 -mu_x_max 1E-8 -mu_y_min -1E-8 -mu_y_max 1E-8 -pi_min -1E-7 -pi_max 1E-7
