cbe9304f8ebe95584017093d9e967ff99131d861
Brian Kloppenborg <brian@kloppenborg.net>
2011-09-20 03:57:29 +0000

    Changes to CMake files for the cluster.
    git-svn-id: http://192.168.1.75/svn/orbit-fit@62 b9008261-a95d-470b-9c38-38f0b39c50e0

--- bin/submit_fitrv.cmd
+++ bin/submit_fitrv.cmd
@@ -2,7 +2,7 @@
 ### Job name
 #PBS -N fitrv
 ### Number of nodes and processors per node.
-#PBS -l nodes=2:ppn=8
+#PBS -l nodes=1:ppn=8
 #PBS -q normal
 #PBS -j oe
 
@@ -17,4 +17,8 @@ echo Dir: $PWD
 echo This jobs runs on the following processors:
 cat $PBS_NODEFILE
 # openmpi test
-mpirun -np 16 -machinefile $PBS_NODEFILE mpirun -np 2 ./fitrv ../test_data/epsAur_rv_noeclipse.txt -gamma_min -10 -gamma_max 10 -K_min 5 -K_max 20 -tau_min 2445400 -tau_max 2455400 -T_min 8000 -T_max 12000 -err 1 -s_min 0 -s_max 20 -turb
+
+echo fitrv_turb
+
+mpirun -np 8 -machinefile $PBS_NODEFILE ./fitrv ../test_data/epsAur_rv_noeclipse.txt -gamma_min -10 -gamma_max 10 -K_min 5 -K_max 20 -tau_min 2445400 -tau_max 2455400 -T_min 8000 -T_max 12000 -rv_err 1 -turb -rv_s_min 0 -rv_s_max 20
+
