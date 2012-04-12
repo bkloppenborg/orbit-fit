d651b728c670c92aa52512163bac45068ec83dad
Brian Kloppenborg <brian@kloppenborg.net>
2012-04-12 20:56:41 +0000

    Changed submission script.
    git-svn-id: http://192.168.1.75/svn/orbit-fit@89 b9008261-a95d-470b-9c38-38f0b39c50e0

--- scripts/submit_fitboth.cmd
+++ scripts/submit_fitboth.cmd
@@ -20,8 +20,9 @@ cat $PBS_NODEFILE
 
 FITBOTH=../bin/fitboth
 
-OUTPUT=fitboth_tycho2-normal
-AST_FILE=epsAur_ast_${OUTPUT}.txt
+INPUT=ppmx-refit
+OUTPUT=fitboth_${INPUT}-
+AST_FILE=epsAur_ast_${INPUT}.txt
 
 tau_min=2413932
 tau_max=2454878
@@ -30,5 +31,5 @@ Omega_max=117.5
 inc_min=89
 inc_max=89.5
 
-mpirun -np 8 -machinefile $PBS_NODEFILE ${FITBOTH} ../test_data/epsAur_rv_noeclipse.txt ../test_data/${AST_FILE} -gamma_min -10 -gamma_max 10 -K_min 5 -K_max 20 -tau_min ${tau_min} -tau_max ${tau_max} -T_min 9000 -T_max 11000 -rv_err 1 -turb -rv_s_min 0 -rv_s_max 20 -alpha_min 1E-3 -alpha_max 7E-2 -inc_min ${inc_min} -inc_max ${inc_max} -Omega_min ${Omega_min} -Omega_max ${Omega_max} -motion -x0_min -25 -x0_max -23 -y0_min 74 -y0_max 76 -mu_x_min -1E-4 -mu_x_max 1E-4 -mu_y_min -1E-4 -mu_y_max 1E-4 -pi_min -1E-1 -pi_max 1E-1 -noerr -ast_err 0.04
+mpirun -np 8 -machinefile $PBS_NODEFILE ${FITBOTH} ../test_data/epsAur_rv_noeclipse.txt ../test_data/${AST_FILE} -o ${OUTPUT} -gamma_min -10 -gamma_max 10 -K_min 5 -K_max 20 -tau_min ${tau_min} -tau_max ${tau_max} -T_min 9000 -T_max 11000 -rv_err 1 -turb -rv_s_min 0 -rv_s_max 20 -alpha_min 1E-3 -alpha_max 7E-2 -inc_min ${inc_min} -inc_max ${inc_max} -Omega_min ${Omega_min} -Omega_max ${Omega_max} -motion -x0_min -25 -x0_max -23 -y0_min 74 -y0_max 76 -mu_x_min -1E-4 -mu_x_max 1E-4 -mu_y_min -1E-4 -mu_y_max 1E-4 -pi_min -1E-1 -pi_max 1E-1 -noerr -ast_err 0.04
 
